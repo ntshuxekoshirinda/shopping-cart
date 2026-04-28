e3a8d5eaad186eeda5c7f40769c765a61253fbef
ntshuxeko shirinda <ntshuxekoshirinda96@gmail.com>
2026-04-28 11:04:13 +0200

    image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
+import boutiqueHero from './assets/fav.png';
 const Home = () => {
   return (
     <div className="home-page">
       <h2>Elevated Essentials</h2>
       <p>Explore our curated collection of high-quality goods.</p>
       <div className="hero-image">
-        <img src="src\assets\fav.png" alt="BOUTIQUE " />
+        
+        <img
+          alt="BOUTIQUE"
+          src={boutiqueHero}
+          />
       </div>
     </div>
   );
